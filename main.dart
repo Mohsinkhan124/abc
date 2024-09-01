50d521e14a9e11cce50622060ebc9c00a95fb464
Mohsinkhan124 <Mohsinkhantwy@gmail.com>
2024-09-01 05:46:51 -0700

    first commit

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+  List mylist = [1, 2, 3, 4, 5, 6];
+// replace the item and index
+  // mylist[2] = 9;
+  // print(mylist);
+  // replace multiple
+  // mylist.replaceRange(0, 4, [12, 13, 14, 15, 16, 17]);
+  // print(mylist);
+
+  // var vals = [1,2,3,4,5,6,7,];
+  //vals.remove(2);
+  // print(vals);
+
+  /// vals.removeAt(vals.length - 5);
+  // print(vals);
+
+  // vals.removeLast();
+  // print(vals);
+
+  var data = {'name': 'mohsin khan' 'age' '18'};
+
+  print(data);
+}
