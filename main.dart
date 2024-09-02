abeabf61062facccb476021c9e96912a2f06689d
Mohsinkhan124 <Mohsinkhantwy@gmail.com>
2024-09-02 12:40:38 -0700

    code

--- main.dart
+++ main.dart
@@ -1,23 +1,8 @@
 void main() {
-  List mylist = [1, 2, 3, 4, 5, 6];
-// replace the item and index
-  // mylist[2] = 9;
-  // print(mylist);
-  // replace multiple
-  // mylist.replaceRange(0, 4, [12, 13, 14, 15, 16, 17]);
-  // print(mylist);
+  var vals = [1, 2, 3, 4, 5];
+  vals.remove(4);
+  print(vals);
 
-  // var vals = [1,2,3,4,5,6,7,];
-  //vals.remove(2);
-  // print(vals);
-
-  /// vals.removeAt(vals.length - 5);
-  // print(vals);
-
-  // vals.removeLast();
-  // print(vals);
-
-  var data = {'name': 'mohsin khan' 'age' '18'};
-
-  print(data);
+  vals.removeAt(vals.length - 1);
+  print(vals);
 }
